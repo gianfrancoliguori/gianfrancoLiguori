31b7d3320495b2997696e169ecc81079db172e46
Gian <gianfrancoliguori001@gmail.com>
2023-08-16 11:25:17 -0300

    Ejercicios complementarios

---
+++ EjerciciosComplementarios.py
@@ -0,0 +1,46 @@
+#1
+numero1 = 2
+#2
+numero2 = 3.5
+#3
+suma = numero1 + numero2
+#4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+
+#5
+nombre = "Gian"
+#6
+precio = 8.99
+#7
+descuento = 0.5
+#8 
+precio_final = precio * descuento
+#9
+cadena = "UNIVERSIDAD TECNOLOGICA NACIONAL"
+#10
+longitud = len(cadena)
+#11
+precio = int(precio)
+#12
+nombre = "Lionel"
+apellido = "Messi"
+nombre_completo = nombre + apellido
+#13
+edad = 22
+edad_1 = edad + 5
+edad_2 = edad_1 - 10
+#14
+altura = 1.76 
+altura_1 = (altura * 4) / 3
+#15
+nombre = "GIAN"
+nombre_min = nombre.lower()
+#16
+nombre_min = nombre[0] + nombre[1:].lower()
